a93eacda02cdd75d712dd58aa24f20b931a97d3a
真夏 <867300915@qq.com>
2024-08-23 11:01:20 +0800

    Add files via upload

---
+++ src_2/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- html -->
+    <div class="app">
+        你好~
+    </div>
+    <Person></Person>
+</template>
+<script>
+    import Person from './components/Person.vue';
+    export default {
+        name: 'App',
+        components:{Person}//注册组件
+    }
+</script>
+<style>
+    .app {
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+</style>
---
+++ src_2/components/Person.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="person">
+        <div>姓名：{{ name }}</div>
+        <div>年龄：{{ age }}</div>
+        <!-- <div>电话：{{ tel }}</div> -->
+         <button @click="showTel()">查看联系方式</button>
+         <button @click="updateName()">修改名字</button>
+         <button @click="updateAge()">修改年龄</button>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Person',
+        data(){
+            return {
+                name: "Tom",
+                age: 12,
+                tel: 1234567
+            }
+        },
+        methods:{
+            showTel(){
+                alert(this.tel)
+            },
+            updateAge(){
+                this.age = 100
+            },
+            updateName(){
+                this.name = "hahaha"
+            }
+        }
+    }
+</script>
+
+<style>
+    .person {
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button {
+        margin: 0 5px;
+    }
+</style>
